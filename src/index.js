72a84cfa980b44252a61a49825cad54ee059c2c4
HunterTigerX <HunterTigerX@gmail.com>
2021-08-22 23:28:23 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,8 +38,35 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let finalResult = "";
+    let exprArray = expr.match(/.{1,10}/g);
+    let copyMorse = Object.assign({}, MORSE_TABLE);
+    let arrayMorse = Object.entries(copyMorse);
+    for (let i = 0; i < arrayMorse.length; i++) {
+        arrayMorse[i][0] = arrayMorse[i][0].replace(/-/g, 11);
+        arrayMorse[i][0] = arrayMorse[i][0].replace(/\./g, 10);
+    if (arrayMorse[i][0].length < 10) {
+    while (arrayMorse[i][0].length < 10) {
+        arrayMorse[i][0] = arrayMorse[i][0].split("");
+        arrayMorse[i][0].unshift("0");
+        arrayMorse[i][0] = arrayMorse[i][0].join("");
+        }
+    }   
 }
+    for (let a = 0; a < exprArray.length; a++) {
+        if (exprArray[a][0] === "*" && exprArray[a][0] !== 1 && exprArray[a][0] !== 0) {
+            finalResult += " ";
+        } else {
+        for (b = 0; b < arrayMorse.length; b++) {
+            if (exprArray[a] === arrayMorse[b][0]) {
+                finalResult += arrayMorse[b][1];
+            }
+        } 
+    }}
+return finalResult;
+}
+
+
 
 module.exports = {
     decode
